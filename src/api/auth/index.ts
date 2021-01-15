d1f88c4fac411f4eee7aa281396f559e05fe7df2
13circle <13circle97@gmail.com>
2021-01-15 23:50:15 +0900

    Fix: Add checkObjectId & checkLoggedIn middleware

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,5 +1,6 @@
 import Router from 'koa-router';
 
+import checkLoggedIn from '../../lib/checkLoggedIn';
 import checkObjectId from '../../lib/checkObjectId';
 
 import * as authCtrl from './auth.ctrl';
@@ -11,7 +12,7 @@ auth.post('/login', authCtrl.login);
 auth.get('/check', authCtrl.check);
 auth.post('/logout', authCtrl.logout);
 
-auth.get('/email-check/:id/:token', checkObjectId, authCtrl.emailCheck);
-auth.patch('/modify/:id', checkObjectId, authCtrl.modify);
+auth.post('/email-check/:id/:token', checkObjectId, authCtrl.emailCheck);
+auth.patch('/modify/:id', checkLoggedIn, checkObjectId, authCtrl.modify);
 
 export default auth;
