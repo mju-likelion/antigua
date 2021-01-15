70d8e6a1ff65787dc742e4d5603a95cccbb2b4a9
13circle <13circle97@gmail.com>
2021-01-15 23:57:46 +0900

    Feat: List unapproved users & approve user by id

--- src/api/admin/admin.ctrl.ts
+++ src/api/admin/admin.ctrl.ts
@@ -1,15 +1,72 @@
 import { RouterContext } from 'koa-router';
 
-import User from '../../models/user';
+import Admin from '../../models/admin';
+import User, { IUser } from '../../models/user';
 
-// TODO: user approval 관리자 승인 만들기
-
-// GET /api/admin/wating-account-list
-// eslint-disable-next-line
-export const waitingAccountList = async (ctx: RouterContext): Promise<void> => {
+// 미승인 회원 목록
+// GET /api/admin/list-unapproved
+export const listUnapproved = async (ctx: RouterContext): Promise<void> => {
   try {
-    const waitingList = await User.find({ accountConfirmed: false });
-    console.log(waitingList);
+    const unapproved = await User.find({
+      accountConfirmed: false,
+    }).exec();
+
+    ctx.body = unapproved.map((user: IUser) => user.serialize());
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+// 회원 승인
+// POST /api/admin/approve/:id
+export const approve = async (ctx: RouterContext): Promise<void> => {
+  const { id } = ctx.params;
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      ctx.status = 400;
+      return;
+    }
+
+    if (!user.emailConfirmed) {
+      ctx.status = 401;
+      return;
+    }
+
+    if (user.accountConfirmed) {
+      ctx.status = 409;
+      return;
+    }
+
+    await user.approve();
+
+    ctx.body = user.serialize();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+// Admin 승격 (aka 개구멍)
+// POST /api/admin/to-admin
+// TODO: 운영진 등록 후 반드시 지울 것!
+export const toAdmin = async (ctx: RouterContext): Promise<void> => {
+  try {
+    const user = await User.findById(ctx.state.user.id);
+
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+
+    const admin = new Admin({
+      userId: user,
+    });
+
+    await admin.save();
+
+    ctx.body = user.serialize();
   } catch (e) {
     ctx.throw(500, e);
   }
