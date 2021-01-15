f9e1a0c6a7cccd90312763dc44223076a4f36c61
13circle <13circle97@gmail.com>
2021-01-16 02:01:36 +0900

    Fix: Change method for email-check (POST -> GET)
    - For URL access in browser

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -12,7 +12,7 @@ auth.post('/login', authCtrl.login);
 auth.get('/check', authCtrl.check);
 auth.post('/logout', authCtrl.logout);
 
-auth.post('/email-check/:id/:token', checkObjectId, authCtrl.emailCheck);
+auth.get('/email-check/:id/:token', checkObjectId, authCtrl.emailCheck);
 auth.patch('/modify/:id', checkLoggedIn, checkObjectId, authCtrl.modify);
 
 export default auth;
