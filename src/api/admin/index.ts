ce71f31ae287da3078ee254b75b150704339aac8
13circle <13circle97@gmail.com>
2021-01-15 23:57:46 +0900

    Feat: List unapproved users & approve user by id

--- src/api/admin/index.ts
+++ src/api/admin/index.ts
@@ -1,9 +1,29 @@
 import Router from 'koa-router';
 
+import checkAdmin from '../../lib/checkAdmin';
+import checkLoggedIn from '../../lib/checkLoggedIn';
+import checkObjectId from '../../lib/checkObjectId';
+
 import * as adminCtrl from './admin.ctrl';
 
 const admin = new Router();
 
-admin.get('/wating-account-list', adminCtrl.waitingAccountList);
+admin.get(
+  '/list-unapproved',
+  checkLoggedIn,
+  checkAdmin,
+  adminCtrl.listUnapproved,
+);
+admin.post(
+  '/approve/:id',
+  checkLoggedIn,
+  checkAdmin,
+  checkObjectId,
+  adminCtrl.approve,
+);
+
+// Admin 승격 API (aka 개구멍)
+// TODO: 운영진 등록 후 반드시 지울 것!
+admin.post('/to-admin', checkLoggedIn, adminCtrl.toAdmin);
 
 export default admin;
