6fbf6c6df43b1fec8adc06bfe1601fd39c2be7a9
13circle <13circle97@gmail.com>
2021-01-17 17:53:53 +0900

    Fix: Add checkLoggedIn middleware

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -9,8 +9,8 @@ const auth = new Router();
 
 auth.post('/register', authCtrl.register);
 auth.post('/login', authCtrl.login);
-auth.get('/check', authCtrl.check);
-auth.post('/logout', authCtrl.logout);
+auth.get('/check', checkLoggedIn, authCtrl.check);
+auth.post('/logout', checkLoggedIn, authCtrl.logout);
 
 auth.get('/email-check/:id/:token', checkObjectId, authCtrl.emailCheck);
 auth.patch('/modify/:id', checkLoggedIn, checkObjectId, authCtrl.modify);
