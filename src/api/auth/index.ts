3559032ae0a4bf71d395a6abb026167b21913217
Noah <noa316@naver.com>
2021-01-17 18:07:40 +0900

    Fix: Change method for email-check (GET -> POST) - GET method should never modify the DataBase.

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -12,7 +12,7 @@ auth.post('/login', authCtrl.login);
 auth.get('/check', checkLoggedIn, authCtrl.check);
 auth.post('/logout', checkLoggedIn, authCtrl.logout);
 
-auth.get('/email-check/:id/:token', checkObjectId, authCtrl.emailCheck);
+auth.post('/email-check/:id/:token', checkObjectId, authCtrl.emailCheck);
 auth.patch('/modify/:id', checkLoggedIn, checkObjectId, authCtrl.modify);
 
 export default auth;
